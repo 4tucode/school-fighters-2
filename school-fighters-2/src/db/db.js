3763357ae0a37ef240b9c3b6107d41b6f5557092
4tucode <4tucode@gmail.com>
2025-01-14 14:03:54 +0100

    Completado el diseño de la VistaPersonajes

---
+++ school-fighters-2/src/db/db.js
@@ -0,0 +1,10 @@
+export const personajes = [
+    {
+        nombre: 'Soldado Romano',
+        imagen: '@/assets/img/combate.gif'
+    },
+    {
+        nombre: 'Soldado Bárbaro',
+        imagen: '@/assets/img/combate.gif'
+    },
+]
--- school-fighters-2/src/views/VistaEscenarios.vue
+++ school-fighters-2/src/views/VistaEscenarios.vue
@@ -1,11 +1,27 @@
 <template>
-    <section>
-      
+    <section class="h-screen flex justify-center items-center flex-col bg-cover landscape:bg-[url('../assets/img/fondo_personajes.png')]">
+      <h1 class="text-center text-white text-5xl p-2 font-bold">SCHOOL FIGHTER 2</h1>
+      <p class="w-[60%] text-center text-white p-5">El mejor juego de lucha entre alumnos. Selecciona el escenario que más te guste.</p>
+      <div class="flex w-full justify-center items-center gap-6">
+        <!-- <select v-model="personaje1" class="bg-white border border-gray-300 text-sm rounded-lg focus:ring-blue-500 block w-1/3 p-2.5">
+          <option disabled value="">Selecciona un personaje</option>
+          <option v-for="(personaje, index) in personajes" :key="index" :value="index">{{personaje.nombre}}</option>
+        </select> -->
+      </div>
+      <!-- <div class="flex justify-center my-5">
+        <button class="bg-[#3DBC1E] text-white px-4 py-2 rounded font-bold">Continuar</button>
+      </div> -->
     </section>
   </template>
   
   <script>
   export default {
-      name: 'VistaEscenarios'
-  }
+    name: 'VistaPersonajes',
+    data() {
+      return {
+        escenario: '',
+      };
+    },
+  };
   </script>
+  
